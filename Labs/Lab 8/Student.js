3a89c254ed9b1fb2a1fefe06a6751a8028e11ce4
Asteraxa <60528663+Asteraxa@users.noreply.github.com>
2021-11-11 19:23:03 -0500

    Delete Student.js

--- Labs/Lab 8/Student.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const studentSchema = new mongoose.Schema({
-    firstName: String, 
-    lastName: String
-})
-
-module.exports = mongoose.model("Student", studentSchema);
